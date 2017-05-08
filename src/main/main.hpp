abbab725424dfba1c8a8666102c768e8f27ee23d
tehryn <thetehryn@gmail.com>
2017-05-08 21:25:12 +0200

    main page

---
+++ src/main/main.hpp
@@ -0,0 +1,17 @@
+/*! \mainpage Solitaire game, project to ICP 2017
+ *
+ * \section intro_sec Introduction
+ *
+ * In this page, you can find documentation of whole project.
+ *
+ * \section install_sec Installation
+ *
+ * To install application, you need to have QT 5 installated on your computer (with qmake). <br>
+ * Since you have qmake and QT 5 with qmake, you can run "make" from root directory of project. <br>
+ * Binnary files are stored in src directory (./src/hra2017 and ./src/hra2017-cli).
+ * You can run both applications with "make run" <br>
+ * Enjoy the game and have fun!
+ *
+ * \section authors Authors
+ * Game was developed by Matejka Jiri and Misova Miroslava.
+ */
