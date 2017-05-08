676d7c87cc40b58b91fd60a65b7c76ce8961869a
tehryn <thetehryn@gmail.com>
2017-05-08 21:09:17 +0200

    dokumentace

---
+++ doc/html/search/all_10.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['_7eg_5fboard',['~G_Board',['../classG__Board.html#acbd46a5e611a6b4f97362ca0364850b7',1,'G_Board']]],
+  ['_7emain_5fwindow',['~Main_Window',['../classMain__Window.html#a2a0da74479f0f03390319de6fb6cfe23',1,'Main_Window']]]
+];
